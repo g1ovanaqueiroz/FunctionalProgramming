6447a996ff959bdabcb75c05d5d244389d9ff201
g1ovanaqueiroz <giovana.oliveira@ccc.ufcg.edu.br>
2022-06-07 22:23:08 -0300

    fatorial

--- primeiro-estagio/lambda-calculus/lambda.hs
+++ primeiro-estagio/lambda-calculus/lambda.hs
@@ -4,7 +4,7 @@ square = \x -> x*x
 --Implemente as funções anteriormente escritas usando expressões lambda
 --consulte suas implementacoes anteriores para a documentacao dessas funcoes
 pow = \x y -> x**y
-fatorial x = undefined
+fatorial = \x -> if (x == 0) then 1 else (x * fatorial (x-1))
 isPrime x = undefined
 fib x = undefined
 mdc x y = undefined
